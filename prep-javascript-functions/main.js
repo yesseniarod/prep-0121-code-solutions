c677e3cda9a002a50c3ede025a5ab711dbf7cbe8
Yessenia Rodriguez <yessi.rod30@gmail.com>
2021-01-13 14:50:00 -0800

    Completed convertHoursToMinutes and personalizeGreeting

--- prep-javascript-functions/main.js
+++ prep-javascript-functions/main.js
@@ -4,3 +4,15 @@ function addTwoNumbers(num1, num2) {
 
 var addTwoNumbersResult = addTwoNumbers(2, 2);
 console.log('addTwoNumbers Exercise:', addTwoNumbersResult);
+
+function convertHoursToMinutes(num) {
+  return num * 60;
+}
+var convertHoursToMinutesResults = convertHoursToMinutes(2);
+console.log('convertHoursToMinutes Exercise:', convertHoursToMinutesResults);
+
+function personalizeGreeting(str) {
+  return 'Hello ' + str + '!';
+}
+var personalizeGreetingResult = personalizeGreeting('World');
+console.log('personalizeGreeting Exercise:', personalizeGreetingResult);
